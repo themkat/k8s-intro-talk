946f116f648d755d396c2f2b42bd35b5c847680c
Marie Katrine Ekeberg <spacemarky@gmail.com>
2019-08-20 20:55:20 +0200

    Added CrossOrigin to the bookservice in case people want to run it on their machine without k8s :) (good if you want to extend or debug it)

--- example/bookservice/src/main/kotlin/net/themkat/karta/k8s/books/controller/BookController.kt
+++ example/bookservice/src/main/kotlin/net/themkat/karta/k8s/books/controller/BookController.kt
@@ -2,12 +2,14 @@ package net.themkat.karta.k8s.books.controller
 
 import net.themkat.karta.k8s.books.model.Book
 import net.themkat.karta.k8s.books.service.BookService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class BookController(val bookService: BookService) {
 
     @GetMapping("/books")
